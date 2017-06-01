105c4878f3c7d4cad9a8729ede6b1e08dfe80044
Khrystyna Hospodarysko <khospodarysko@tidemark.com>
2017-06-01 11:49:16 +0300

    Removed unused import

--- realtor-service/src/test/java/com/lohika/jclub/RealtorServiceApplicationTests.java
+++ realtor-service/src/test/java/com/lohika/jclub/RealtorServiceApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
