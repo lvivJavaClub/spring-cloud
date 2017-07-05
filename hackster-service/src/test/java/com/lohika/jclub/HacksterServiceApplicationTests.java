9b3b8ddab5b139a97faec75c491652a1645d75da
Andriy Rymar <andriyrav@gmail.com>
2017-07-05 11:51:31 +0300

    Added tests

--- hackster-service/src/test/java/com/lohika/jclub/HacksterServiceApplicationTests.java
+++ hackster-service/src/test/java/com/lohika/jclub/HacksterServiceApplicationTests.java
@@ -2,14 +2,50 @@ package com.lohika.jclub;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HacksterServiceApplicationTests {
+  @Value("${maxAllowedApartmentsPerRealtor}")
+  private int maxAllowedApartmentsPerRealtor;
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void checkIfNewPhoneNumberIsNotHakster() throws Exception {
+    mockMvc.perform(get("/hackster/123123123"))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("false"));
+  }
+
+  @Test
+  public void checkIfOldPhoneNumberIsNotHakster() throws Exception {
+    mockMvc.perform(get("/hackster/321321"))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("false"));
+
+    for (int i = 0; i < maxAllowedApartmentsPerRealtor - 1; i++) {
+      mockMvc.perform(get("/hackster/321321"));
+    }
+
+    mockMvc.perform(get("/hackster/321321"))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("true"));
+  }
 }
