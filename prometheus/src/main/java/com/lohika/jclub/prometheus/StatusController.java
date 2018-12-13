5f9f034b6b2336183ba997e8f00c575b901f92c9
Serge Bishyr <seriybg@gmail.com>
2018-12-13 10:41:38 +0200

    add metrics by status

--- prometheus/src/main/java/com/lohika/jclub/prometheus/StatusController.java
+++ prometheus/src/main/java/com/lohika/jclub/prometheus/StatusController.java
@@ -1,7 +1,10 @@
 package com.lohika.jclub.prometheus;
 
+import java.io.IOException;
+import java.io.Writer;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.exporter.common.TextFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +12,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnablePrometheusEndpoint
+//@EnablePrometheusEndpoint
 public class StatusController {
 
     private final Counter counter = Counter.build()
             .name("http_total_request")
+            .labelNames("status")
             .help("All http calls")
             .register();
 
     @GetMapping("/status/{status}/{content}")
     public ResponseEntity<String> getStatus(@PathVariable int status, @PathVariable String content) {
-        counter.inc();
+        counter.labels(String.valueOf(status)).inc();
         return new ResponseEntity<>(content, HttpStatus.resolve(status));
+    }
+
+    @GetMapping("/metrics")
+    public void metrics(Writer writer) throws IOException {
+        TextFormat.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
+        writer.close();
     }
 }
