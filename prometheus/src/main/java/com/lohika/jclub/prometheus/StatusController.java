4072dd13aa1c1d20f779fd9dba5c84149d1eb74d
Serge Bishyr <seriybg@gmail.com>
2018-12-13 10:23:13 +0200

    add prometheus monitoring

--- prometheus/src/main/java/com/lohika/jclub/prometheus/StatusController.java
+++ prometheus/src/main/java/com/lohika/jclub/prometheus/StatusController.java
@@ -1,5 +1,7 @@
 package com.lohika.jclub.prometheus;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnablePrometheusEndpoint
 public class StatusController {
+
+    private final Counter counter = Counter.build()
+            .name("http_total_request")
+            .help("All http calls")
+            .register();
 
     @GetMapping("/status/{status}/{content}")
     public ResponseEntity<String> getStatus(@PathVariable int status, @PathVariable String content) {
+        counter.inc();
         return new ResponseEntity<>(content, HttpStatus.resolve(status));
     }
 }
